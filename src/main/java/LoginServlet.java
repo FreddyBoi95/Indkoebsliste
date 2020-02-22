2f412030f6ae291d6e957ec23090084e4e0fb898
Frederik Janum <cph-fj108@cphbusiness.dk>
2020-02-22 15:33:32 +0100

    login er sgu nok ved at være ok

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,17 +1,54 @@
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.getWriter().println("Du har ramt loginServletten");
+        //Holder forbindelsen så længe serveren kører (applikation scope)
+        ServletContext servletContext = getServletContext();
 
+        String navn = request.getParameter("Navn");
+        String kodeord = request.getParameter("Kodeord");
+
+        if (servletContext.getAttribute("brugerMap") == null) {
+            //Lav et map af navne, hvis der ikke er nogle
+            Map<String, String> brugerMap = new HashMap<>();
+
+            //Testbruger
+            brugerMap.put("test","test");
+
+            servletContext.setAttribute("brugerMap", brugerMap);
+        }
+
+        if (!((Map<String, String>) servletContext.getAttribute("brugerMap")).containsKey(navn)) {
+
+            //todo Gå til login side
+            request.setAttribute("besked", "Brugernavnet findes ikke.");
+            request.getRequestDispatcher("index.jsp").forward(request,response);
+
+        }
+
+        if (((Map<String, String>) servletContext.getAttribute("brugerMap")).get(navn).equalsIgnoreCase(kodeord)) {
+
+            //todo Gå til huskelisten
+
+            request.getRequestDispatcher("/WEB-INF/HuskeListe.jsp").forward(request,response);
+        }
+
+        //todo Gå til login dvs. index siden
+
+        request.setAttribute("besked", "Din kode var forkert, prøv igen.");
+        request.getRequestDispatcher("index.jsp").forward(request,response);
     }
+
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
---
+++ src/main/webapp/WEB-INF/HuskeListe.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: frede
+  Date: 22-02-2020
+  Time: 14:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>HuskeListe</title>
+</head>
+<body>
+
+Du har nu adgang til din huskeliste.
+
+
+</body>
+</html>
